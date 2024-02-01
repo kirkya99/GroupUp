191ce2786b7c34fb4c845b9f8303ba01f4a7d3bb
kirkya99 <fabianschotte@outlook.com>
2024-02-01 14:15:53 +0100

    update

--- app/src/main/java/de/rwu/group_up/ui/start_screen/user_profile_creation/UserProfileCreationViewModel.java
+++ app/src/main/java/de/rwu/group_up/ui/start_screen/user_profile_creation/UserProfileCreationViewModel.java
@@ -2,15 +2,18 @@ package de.rwu.group_up.ui.start_screen.user_profile_creation;
 
 import androidx.lifecycle.ViewModel;
 
+import java.util.HashMap;
+
 import de.rwu.group_up.User;
 import de.rwu.group_up.ui.user_profile.view.UserProfileDetailsViewModel;
 
 public class UserProfileCreationViewModel extends ViewModel {
 
     private User newUser;
+    private HashMap<String, Boolean> interestsMap;
 
-
-    public UserProfileDetailsViewModel() {
+    public UserProfileCreationViewModel() {
         newUser = new User();
+        interestsMap = newUser.getInterestsMap();
     }
 }
