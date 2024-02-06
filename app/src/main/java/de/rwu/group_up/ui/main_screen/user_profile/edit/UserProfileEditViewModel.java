0a893b22aeec683e0de4621c963beb0c033a89fb
kirkya99 <fabianschotte@outlook.com>
2024-02-06 15:31:42 +0100

    added view model for user edit

---
+++ app/src/main/java/de/rwu/group_up/ui/main_screen/user_profile/edit/UserProfileEditViewModel.java
@@ -0,0 +1,10 @@
+package de.rwu.group_up.ui.main_screen.user_profile.edit;
+
+import android.view.View;
+
+import androidx.lifecycle.ViewModel;
+
+public class UserProfileEditViewModel extends ViewModel {
+
+
+}
