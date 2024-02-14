fc1e1a78031e3615b16a8ed89a6cacdb60fa3bc4
kirkya99 <fabianschotte@outlook.com>
2024-02-14 16:50:59 +0100

    added buttons for the search of specific groups

--- app/src/main/java/de/rwu/group_up/ui/main_screen/all_groups/list/AllGroupsFragment.java
+++ app/src/main/java/de/rwu/group_up/ui/main_screen/all_groups/list/AllGroupsFragment.java
@@ -2,14 +2,19 @@ package de.rwu.group_up.ui.main_screen.all_groups.list;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.appcompat.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import de.rwu.group_up.R;
 import de.rwu.group_up.databinding.FragmentAllGroupsBinding;
 
 public class AllGroupsFragment extends Fragment {
@@ -33,6 +38,9 @@ public class AllGroupsFragment extends Fragment {
 
         final TextView textView = binding.textNotifications;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        setHasOptionsMenu(true);
+
         return root;
     }
 
@@ -40,5 +48,25 @@ public class AllGroupsFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.groups_menu, menu);
+
+        MenuItem searchItem = menu.findItem((R.id.search_group));
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 }
